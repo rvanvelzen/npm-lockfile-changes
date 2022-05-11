8d8ec036914fa61ec5f9893244458ceaf1fbdceb
Christoph Werner <christoph+github@codepunkt.de>
2022-05-11 21:03:32 +0200

    fix

--- src/action.js
+++ src/action.js
@@ -61,6 +61,7 @@ const run = async () => {
     }
 
     const content = fs.readFileSync(lockPath, { encoding: 'utf8' })
+    console.log({ content })
     const updatedLock = packageLockParser.parseLockFile(content)
 
     const oktokitParams = { owner, repo }
@@ -90,6 +91,8 @@ const run = async () => {
     if (!baseLockData || !baseLockData.data || !baseLockData.data.content) {
       throw Error('💥 Cannot fetch repository base lock file, aborting!')
     }
+
+    console.log({ oldContent: Base64.decode(baseLockData.data.content) })
 
     const baseLock = packageLockParser.parseLockFile(Base64.decode(baseLockData.data.content))
     const lockChanges = diffLocks(baseLock, updatedLock)
